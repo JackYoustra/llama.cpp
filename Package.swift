8756308e4b38acdac0ed9c04318ec7c451e860bf
Jack Youstra <jack@youstra.com>
2023-08-06 17:21:31 +0800

    Add kquants

--- Package.swift
+++ Package.swift
@@ -16,13 +16,14 @@ let package = Package(
             name: "llama",
             path: ".",
             exclude: ["ggml-metal.metal"],
-            sources: ["ggml.c", "llama.cpp", "ggml-metal.m", "examples/common.cpp"],
+            sources: ["ggml.c", "llama.cpp", "ggml-metal.m", "k_quants.c", "examples/common.cpp"],
             publicHeadersPath: "spm-headers",
             cSettings: [
                 .unsafeFlags(["-Wno-shorten-64-to-32"]),
                 .define("GGML_USE_ACCELERATE"),
                 .define("GGML_USE_METAL"),
                 .define("GGML_METAL_NDEBUG"),
+                .define("GGML_USE_K_QUANTS"),
                 .unsafeFlags(["-fno-objc-arc"]),
             ],
             cxxSettings: [
